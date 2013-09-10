5b6988065e2dc22650d259b132164883e33a3f17
Clemens Bergmann <cbergmann@schuhklassert.de>
2013-09-10 13:52:48 +0200

    initial commit

---
+++ PhisingEducation/gen/de/tudarmstadt/informatik/secuso/phisedu/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.tudarmstadt.informatik.secuso.phisedu;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
