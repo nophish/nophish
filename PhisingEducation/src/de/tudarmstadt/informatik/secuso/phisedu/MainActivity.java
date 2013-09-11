696bb1e5e662325a585b03603068a00f6f9156e7
Clemens Bergmann <cbergmann@schuhklassert.de>
2013-09-11 13:28:31 +0200

    first working browser and back example

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phisedu/MainActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phisedu/MainActivity.java
@@ -1,9 +1,12 @@
 package de.tudarmstadt.informatik.secuso.phisedu;
 
 import de.tudarmstadt.informatik.secuso.phisedu.R;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +16,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    public void redirectToPage(View view){
+    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://clemens.schuhklassert.de"));
+    	startActivity(browserIntent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
