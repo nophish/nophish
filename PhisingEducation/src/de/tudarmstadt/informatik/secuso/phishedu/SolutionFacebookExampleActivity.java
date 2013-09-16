94ec8387296205c2f7c1793b02fb0a2534f8c172
Gamze Canova <gamze.canova@googlemail.com>
2013-09-16 08:57:26 +0200

    prototyp GUI --> bis kurz vor "lesson" zu Nonsense und IP URLs

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/SolutionFacebookExampleActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/SolutionFacebookExampleActivity.java
@@ -1,12 +1,10 @@
 package de.tudarmstadt.informatik.secuso.phishedu;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.view.View;
 
 public class SolutionFacebookExampleActivity extends Activity {
 
@@ -14,19 +12,8 @@ public class SolutionFacebookExampleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_solution_facebook_example);
-		// Show the Up button in the action bar.
-		setupActionBar();
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,22 +21,10 @@ public class SolutionFacebookExampleActivity extends Activity {
 		getMenuInflater().inflate(R.menu.solution_facebook_example, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	
+	public void goToWebAddressExerciseIntro(View view){
+		Intent intent = new Intent(this, WebAddressExerciseIntroActivity.class);
+		startActivity(intent);
 	}
 
 }
