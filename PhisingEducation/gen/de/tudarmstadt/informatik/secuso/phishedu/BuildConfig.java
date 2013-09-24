9d3f0de65f261ba06317884cbd48029d23f461f5
Clemens Bergmann <cbergmann@schuhklassert.de>
2013-09-24 20:57:44 +0200

    delete

--- PhisingEducation/gen/de/tudarmstadt/informatik/secuso/phishedu/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.tudarmstadt.informatik.secuso.phishedu;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
