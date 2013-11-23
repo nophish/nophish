351c75822a60e1180d2d2925104cc9c8be6b2c4c
Gamze Canova <gamze.canova@googlemail.com>
2013-11-23 15:25:27 +0100

    score shown on final screen only

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/LevelFinishedActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/LevelFinishedActivity.java
@@ -1,52 +1,40 @@
 package de.tudarmstadt.informatik.secuso.phishedu;
 
 import android.app.ActionBar;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import de.tudarmstadt.informatik.secuso.phishedu.backend.BackendController;
 
 public class LevelFinishedActivity extends SwipeActivity {
 	ActionBar ab;
 	protected static int[][] levelLayoutIds = {
-		{
-			R.layout.level_00_finish_00,
-			R.layout.level_00_finish_01,
-			R.layout.level_00_finish_02,
-			R.layout.level_00_finish_03,
-			R.layout.level_00_finish_04,
-			R.layout.level_00_finish_05,
-			R.layout.level_00_finish_06,
-			R.layout.level_00_finish_07,
-			R.layout.level_00_finish_08
-		},
-		{
-			R.layout.level_01_finish_00,
-		},
-		{
-			R.layout.level_02_finish_00,
-		}
-	};
-	
-	int real_level=0;
-	int index_level=0;
-	
+			{ R.layout.level_00_finish_00, R.layout.level_00_finish_01,
+					R.layout.level_00_finish_02, R.layout.level_00_finish_03,
+					R.layout.level_00_finish_04, R.layout.level_00_finish_05,
+					R.layout.level_00_finish_06, R.layout.level_00_finish_07,
+					R.layout.level_00_finish_08 },
+			{ R.layout.level_01_finish_00, }, { R.layout.level_02_finish_00, } };
+
+	int real_level = 0;
+	int index_level = 0;
+
 	protected void onCreate(Bundle savedInstanceState) {
-		this.real_level = getIntent().getIntExtra(Constants.EXTRA_LEVEL,0);
-		this.index_level=Math.min(real_level,levelLayoutIds.length-1);
-		
+		this.real_level = getIntent().getIntExtra(Constants.EXTRA_LEVEL, 0);
+		this.index_level = Math.min(real_level, levelLayoutIds.length - 1);
+
 		super.onCreate(savedInstanceState);
 	}
 
-	protected void onStartClick(){
-		BackendController.getInstance().startLevel(real_level+1);
+	protected void onStartClick() {
+		BackendController.getInstance().startLevel(real_level + 1);
 	}
 
 	@Override
 	protected String startButtonText() {
-		return "Weiter zu Level "+(real_level+1);
+		return "Weiter zu Level " + (real_level + 1);
 	}
 
 	@Override
@@ -58,28 +46,40 @@ public class LevelFinishedActivity extends SwipeActivity {
 	protected View getPage(int page, LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
 		setTitles();
-		return inflater.inflate(this.levelLayoutIds[index_level][page], container, false);
+		View view = inflater.inflate(this.levelLayoutIds[index_level][page],
+				container, false);
+		setScoreText(view);
+		return view;
 	}
-	
+
+	private void setScoreText(View view) {
+
+		if (index_level > 0) {
+			TextView tv = (TextView) view.findViewById(R.id.total_score);
+			int score = BackendController.getInstance().getPoints();
+			tv.setText(Integer.toString(score));
+		}
+
+	}
+
 	private void setTitles() {
 		ab = getActionBar();
 		String title = getString(Constants.levelTitlesIds[this.real_level]);
 		String subtitle = getString(Constants.levelSubtitlesIds[this.real_level]);
-		
-		if(!title.equals(subtitle)){
-			//if subtitle and title are different, subtitle is set
+
+		if (!title.equals(subtitle)) {
+			// if subtitle and title are different, subtitle is set
 			ab.setSubtitle(subtitle);
 		}
-		//title is set in anyway
+		// title is set in anyway
 		ab.setTitle(title);
 	}
-	
 
 	/**
 	 * after finishing a level the user is not allowed to go back
 	 */
 	@Override
 	public void onBackPressed() {
-		return;		
+		return;
 	}
 }
