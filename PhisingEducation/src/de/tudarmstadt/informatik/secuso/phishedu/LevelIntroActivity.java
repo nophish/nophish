9efab2d640d54738bcec62c074a44ac8a3362198
Gamze Canova <gamze.canova@googlemail.com>
2013-11-30 14:20:04 +0100

    more generalized example string spans in code

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/LevelIntroActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/LevelIntroActivity.java
@@ -25,10 +25,23 @@ public class LevelIntroActivity extends SwipeActivity {
 
 	SpannableStringBuilder strBuilder = new SpannableStringBuilder();
 	int wordStart, wordEnd;
+	protected static String[][] exampleReminderUrlPartId = {
+			{ "http://", "google.com.", "phishers-seite.de",
+					"/search/online+banking+postbank" },
+			{ "http://", "", "192.168.160.02", "/secure-login" },
+			{ "https://", "secure-login.mail.google.com.", "hsezis.de",
+					"/update-account" },
+			{ "https://", "microsoft.com.", "security-update.de", "/update" },
+			{ "http://", "www.", "facebook-login.com", "/" },
+			{ "https://", "www.", "fracebook.com", "/login" },
+			{ "http://", "www.", "mircosoft.com", "/en-us/default.aspx" },
+			{ "https://", "www.", "vvetter.com", "/wetter_aktuell/?code=EUDE" },
+			{ "http://", "phisher.de", "/mail.", "google.com", "/login" } };
 	protected static String[][] exampleUrlPartId = {
 			// level 3
 			// TODO: auslagern in strings.xml
-			{ "http://", "google.com.", "phishers-seite.de", "/search" },
+			{ "http://", "google.com.", "phishers-seite.de",
+					"/search/online+banking+postbank" },
 			{ "http://", "", "192.168.160.02", "/secure-login" },
 			{ "https://", "secure-login.mail.google.com.", "hsezis.de",
 					"/update-account", "http://",
@@ -45,9 +58,9 @@ public class LevelIntroActivity extends SwipeActivity {
 					"http://", "www.", "paypa1.com",
 					"/de/webapps/mpp/privatkunden" },
 			{ "http://", "phisher.de", "/mail.", "google.com", "/login" },
-			{ "https://", "www.", "deutsche-bank.de", "/index.htm",
-			  "https://", "www.", "deutsche-bank.de", "/index.htm",
-			  "https://","facebook.","phisher.de","/secure-login"}
+			{ "https://", "www.", "deutsche-bank.de", "/index.htm", "https://",
+					"www.", "deutsche-bank.de", "/index.htm", "https://",
+					"facebook.", "phisher.de", "/secure-login" }
 
 	};
 
@@ -134,7 +147,8 @@ public class LevelIntroActivity extends SwipeActivity {
 				this.levelLayoutIds[this.index_level][page], container, false);
 
 		// when example screen is showns
-		if (view.findViewById(R.id.recognize_attack) != null) {
+		if ((view.findViewById(R.id.recognize_attack) != null)
+				|| view.findViewById(R.id.reminder_examples) != null) {
 			setExampleSpans(view);
 		}
 		setTitles();
@@ -178,8 +192,68 @@ public class LevelIntroActivity extends SwipeActivity {
 
 		int exampleIndex = BackendController.getInstance().getLevel() - 3;
 		String[] url = exampleUrlPartId[exampleIndex];
-		setSpans(url, view);
+		if (view.findViewById(R.id.recognize_attack) != null) {
+			setSpans(url, view);
+		} else if ((BackendController.getInstance().getLevel() > 3)
+				&& (view.findViewById(R.id.reminder_examples) != null)) {
+			setReminderSpans(view, exampleIndex);
+		}
 
+	}
+
+	private void setReminderSpans(View view, int reminderIndex) {
+
+		int level = BackendController.getInstance().getLevel();
+		if (level > 7) {
+			// two more urls displayed from there
+			reminderIndex = reminderIndex + 2;
+		}
+
+		for (int currentIndex = 0; currentIndex < reminderIndex; currentIndex++) {
+			String[] currentUrl = exampleReminderUrlPartId[currentIndex];
+
+			// Example has other spans
+			if (level == 10 && currentIndex == 8) {
+				strBuilder.clear();
+				strBuilder.clearSpans();
+				setLevel9Span(currentUrl, view);
+				return;
+			}
+
+			for (int i = 0; i < currentUrl.length; i++) {
+
+				int spanIndex = (i % 4);
+				if (spanIndex == 0) {
+					strBuilder.clear();
+					strBuilder.clearSpans();
+				}
+
+				setSingleSpan(currentUrl, BackendController.getInstance()
+						.getLevel(), i, spanIndex);
+
+				// example 1 needs to be set
+				if (currentIndex == 0) {
+					setTextView(view, R.id.example_01);
+				} else if (currentIndex == 1) {
+					// example 2 needs to be set
+					setTextView(view, R.id.example_02);
+				} else if (currentIndex == 2) {
+					// example 3 needs to be set
+					setTextView(view, R.id.example_03);
+				} else if (currentIndex == 3) {
+					setTextView(view, R.id.example_04);
+				} else if (currentIndex == 4) {
+					setTextView(view, R.id.example_05);
+				} else if (currentIndex == 5) {
+					setTextView(view, R.id.example_06);
+				} else if (currentIndex == 6) {
+					setTextView(view, R.id.example_07);
+				} else if (currentIndex == 7) {
+					setTextView(view, R.id.example_08);
+				}
+
+			}
+		}
 	}
 
 	private void setSpans(String[] url, View view) {
@@ -206,75 +280,61 @@ public class LevelIntroActivity extends SwipeActivity {
 				strBuilder.clearSpans();
 			}
 
-			String part = url[i];
-			// 0 at the beginning
-			wordStart = strBuilder.length();
-			wordEnd = wordStart + part.length();
-			strBuilder.append(part);
-
-			if (spanIndex == 1 && level == 3) {
-				// we are in level 3, subdomain needs to be marked
-				// make background light red
-				final BackgroundColorSpan bgc = new BackgroundColorSpan(
-						getResources().getColor(R.color.light_red));
-				strBuilder.setSpan(bgc, wordStart, wordEnd, 0);
-			} else if (spanIndex == 2) {
-				// make background red
-				final BackgroundColorSpan bgc = new BackgroundColorSpan(
-						getResources().getColor(R.color.red));
-				strBuilder.setSpan(bgc, wordStart, wordEnd, 0);
-			} else if (spanIndex == 0 || spanIndex == 3) {
-				// make foregroundcolor grey
-				final ForegroundColorSpan fgc = new ForegroundColorSpan(
-						getResources().getColor(R.color.grey));
-				strBuilder.setSpan(fgc, wordStart, wordEnd, 0);
-
-			} else if (spanIndex == 1) {
-				final ForegroundColorSpan fgc = new ForegroundColorSpan(
-						Color.BLACK);
-				strBuilder.setSpan(fgc, wordStart, wordEnd, 0);
-			}
+			setSingleSpan(url, level, i, spanIndex);
 
 			// example 1 needs to be set
 			if (i == 3) {
-				TextView tv1 = (TextView) view.findViewById(R.id.example_01);
-				if (tv1 != null) {
-					tv1.setText(strBuilder);
-				}
+				setTextView(view, R.id.example_01);
 			}
 			if (i == 7) {
 				// example 2 needs to be set
-				TextView tv2 = (TextView) view.findViewById(R.id.example_02);
-				if (tv2 != null) {
-					tv2.setText(strBuilder);
-				}
+				setTextView(view, R.id.example_02);
 			}
 			if (i == 11) {
 				// example 3 needs to be set
-				TextView tv3 = (TextView) view.findViewById(R.id.example_03);
-				if (tv3 != null) {
-					tv3.setText(strBuilder);
-				}
+				setTextView(view, R.id.example_03);
 			}
 
 			// no need to check in other levels
 			if (BackendController.getInstance().getLevel() == 7) {
 				if (i == 15) {
-					TextView tv4 = (TextView) view
-							.findViewById(R.id.example_04);
-					if (tv4 != null) {
-						tv4.setText(strBuilder);
-					}
+					setTextView(view, R.id.example_04);
 				}
 				if (i == 19) {
-					TextView tv5 = (TextView) view
-							.findViewById(R.id.example_05);
-					if (tv5 != null) {
-						tv5.setText(strBuilder);
-					}
+					setTextView(view, R.id.example_05);
 				}
 
 			}
+		}
+	}
+
+	private void setSingleSpan(String[] url, int level, int i, int spanIndex) {
+		String part = url[i];
+		// 0 at the beginning
+		wordStart = strBuilder.length();
+		wordEnd = wordStart + part.length();
+		strBuilder.append(part);
+
+		if (spanIndex == 1 && level == 3) {
+			// we are in level 3, subdomain needs to be marked
+			// make background light red
+			final BackgroundColorSpan bgc = new BackgroundColorSpan(
+					getResources().getColor(R.color.light_red));
+			strBuilder.setSpan(bgc, wordStart, wordEnd, 0);
+		} else if (spanIndex == 2) {
+			// make background red
+			final BackgroundColorSpan bgc = new BackgroundColorSpan(
+					getResources().getColor(R.color.red));
+			strBuilder.setSpan(bgc, wordStart, wordEnd, 0);
+		} else if (spanIndex == 0 || spanIndex == 3) {
+			// make foregroundcolor grey
+			final ForegroundColorSpan fgc = new ForegroundColorSpan(
+					getResources().getColor(R.color.grey));
+			strBuilder.setSpan(fgc, wordStart, wordEnd, 0);
+
+		} else if (spanIndex == 1) {
+			final ForegroundColorSpan fgc = new ForegroundColorSpan(Color.BLACK);
+			strBuilder.setSpan(fgc, wordStart, wordEnd, 0);
 		}
 	}
 
@@ -311,23 +371,14 @@ public class LevelIntroActivity extends SwipeActivity {
 				strBuilder.setSpan(fgc, wordStart, wordEnd, 0);
 			}
 
-		// example 1 needs to be set
-		if (i == 3) {
-			TextView tv1 = (TextView) view.findViewById(R.id.example_01);
-			if (tv1 != null) {
-				tv1.setText(strBuilder);
+			// example 1 needs to be set
+			if (i == 3) {
+				setTextView(view, R.id.example_01);
+			} else if (i == 7) {
+				setTextView(view, R.id.example_02);
+			} else if (i == 11) {
+				setTextView(view, R.id.example_03);
 			}
-		}else if(i == 7){
-			TextView tv2 = (TextView) view.findViewById(R.id.example_02);
-			if (tv2 != null) {
-				tv2.setText(strBuilder);
-			}
-		}else if(i == 11){
-			TextView tv3 = (TextView) view.findViewById(R.id.example_03);
-			if (tv3 != null) {
-				tv3.setText(strBuilder);
-			}
-		}
 		}
 	}
 
@@ -363,13 +414,24 @@ public class LevelIntroActivity extends SwipeActivity {
 				strBuilder.setSpan(bgc, wordStart, wordEnd, 0);
 			}
 
-			// example 1 needs to be set
-			if (i == 3) {
-				TextView tv1 = (TextView) view.findViewById(R.id.example_01);
-				if (tv1 != null) {
-					tv1.setText(strBuilder);
+			if (i == url.length - 1) {
+				// example needs to be set
+				if (view.findViewById(R.id.recognize_attack) != null) {
+					// we are in recognize attack
+					setTextView(view, R.id.example_01);
+
+				} else {
+					// we are in reminder_examples
+					setTextView(view, R.id.example_09);
 				}
 			}
+		}
+	}
+
+	private void setTextView(View view, int resourceId) {
+		TextView tv = (TextView) view.findViewById(resourceId);
+		if (tv != null) {
+			tv.setText(strBuilder);
 		}
 	}
 
