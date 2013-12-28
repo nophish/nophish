445d5e7e5d145d9141b63bc19efc9abf8e442757
Gamze Canova <gamze.canova@googlemail.com>
2013-12-28 21:39:16 +0100

    secuso logo links to secuso website

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/AboutActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/AboutActivity.java
@@ -1,9 +1,12 @@
 package de.tudarmstadt.informatik.secuso.phishedu;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.view.MenuItem;
+import android.view.View;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 
@@ -25,6 +28,12 @@ public class AboutActivity extends PhishBaseActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void openSecusoLink(View view){
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+		browserIntent.setData(Uri.parse("https://www.secuso.informatik.tu-darmstadt.de"));
+		startActivity(browserIntent);
 	}
 
 }
