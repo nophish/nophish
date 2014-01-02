1552d051cfd1c8949a4ba9425b289e2c9ee36871
Clemens Bergmann <cbergmann@schuhklassert.de>
2014-01-02 14:31:42 +0100

    reorder links

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/MoreInfoActivity.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/MoreInfoActivity.java
@@ -27,9 +27,9 @@ public class MoreInfoActivity extends PhishBaseActivity {
 		View v = inflater.inflate(R.layout.more_info_list_view, container, false);
 		
 		entrysForMoreInfo = new LinkedHashMap<String, MoreInfo>();
-		entrysForMoreInfo.put(getString(R.string.info_security_comic),new MoreInfo() {public void pressed() {openLink(getString(R.string.url_securitycomic));}});
-		entrysForMoreInfo.put(getString(R.string.info_apwg), new MoreInfo(){public void pressed(){openLink(getString(R.string.url_apwg));}});
 		entrysForMoreInfo.put(getString(R.string.info_how_phishing_works), new MoreInfo(){public void pressed(){openLink(getString(R.string.url_how_phishing_works));}});
+		entrysForMoreInfo.put(getString(R.string.info_apwg), new MoreInfo(){public void pressed(){openLink(getString(R.string.url_apwg));}});
+		entrysForMoreInfo.put(getString(R.string.info_security_comic),new MoreInfo() {public void pressed() {openLink(getString(R.string.url_securitycomic));}});
 		
 		adapterForMoreInfo = new ArrayAdapter<String>(getActivity(),
 				android.R.layout.simple_list_item_1, entrysForMoreInfo.keySet()
