b068944568930d0423c3770624155646b1665163
Clemens Bergmann <cbergmann@schuhklassert.de>
2014-01-03 06:50:35 +0100

    add KeepAttack

---
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/KeepAttack.java
@@ -0,0 +1,25 @@
+package de.tudarmstadt.informatik.secuso.phishedu.backend.attacks;
+
+
+import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishAttackType;
+import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishURL;
+
+/**
+ * This Attack is a Attack that does no attack.
+ * This is for simplyfing the URL generation.
+ * @author Clemens Bergmann <cbergmann@schuhklassert.de>
+ *
+ */
+public class KeepAttack extends AbstractAttack {
+
+	/**
+	 * See {@link AbstractAttack#AbstractAttack(PhishURL)}
+	 * @param object See {@link AbstractAttack#AbstractAttack(PhishURL)}
+	 */
+	public KeepAttack(PhishURL object) {super(object);}
+	
+	public PhishAttackType getAttackType() {
+		return object.getAttackType();
+	}
+	
+}
