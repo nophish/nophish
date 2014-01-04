ec8ed384584e14f4eabfe87a3a80b22dbeaa161c
Clemens Bergmann <cbergmann@schuhklassert.de>
2014-01-04 13:06:18 +0100

    disable debug

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/Constants.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	/**
 	 * Game configuration
 	 */
-	public static final boolean UNLOCK_ALL_LEVELS = true;
+	public static final boolean UNLOCK_ALL_LEVELS = false;
 
 	public static final String ARG_PAGE_NUMBER = "page";
 	public static final String ARG_RESULT = "result";
