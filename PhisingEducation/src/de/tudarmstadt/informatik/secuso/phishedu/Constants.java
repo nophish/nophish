660c41b4090237898c842fcbe820fb4abd71b566
Clemens Bergmann <cbergmann@schuhklassert.de>
2014-01-06 10:19:42 +0100

    switch debug off

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/Constants.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/Constants.java
@@ -5,8 +5,8 @@ public class Constants {
 	/**
 	 * Game configuration
 	 */
-	public static final boolean UNLOCK_ALL_LEVELS = true;
-	public static final boolean ALLOW_REPEAT_AWARENESS = true;
+	public static final boolean UNLOCK_ALL_LEVELS = false;
+	public static final boolean ALLOW_REPEAT_AWARENESS = false;
 	public static final boolean ALLOW_SKIP_AWARENESS = true;
 	public static final boolean SHOW_GMAIL_BUTTON = true;
 	public static final boolean SKIP_LEVEL1 = true;
