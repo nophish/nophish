9c514270b99aaf88943300a850adea104e1b5155
gcanova <gamze.canova@googlemail.com>
2014-08-03 21:17:18 +0200

    IP added to host in path

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/HostInPathAttack.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/HostInPathAttack.java
@@ -3,6 +3,7 @@ package de.tudarmstadt.informatik.secuso.phishedu.backend.attacks;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import de.tudarmstadt.informatik.secuso.phishedu.backend.BackendControllerImpl;
 import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishAttackType;
@@ -10,39 +11,67 @@ import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishURL;
 
 /**
  * This Attack replaces the whole URL by a replace from phishtank
+ * 
  * @author Clemens Bergmann <cbergmann@schuhklassert.de>
- *
+ * 
  */
 public class HostInPathAttack extends AbstractAttack {
-	
+
 	protected static final String[] PHISHER_DOMAINS = SubdomainAttack.PHISHER_DOMAINS;
-	
+
 	/**
 	 * To build an attack we need a url to decorate
-	 * @param object the decorated URL
+	 * 
+	 * @param object
+	 *            the decorated URL
 	 */
 	public HostInPathAttack(PhishURL object) {
 		super(object);
-		phish_domain=BackendControllerImpl.getInstance().getRandom().nextInt(PHISHER_DOMAINS.length);
+		// if phish_domain = PHISHER_DOMAINS.length --> IP Adress attack with
+		// host in path
+		phish_domain = BackendControllerImpl.getInstance().getRandom()
+				.nextInt(PHISHER_DOMAINS.length + 1);
 	}
 
 	@Override
 	public PhishAttackType getAttackType() {
 		return PhishAttackType.HostInPath;
 	}
-	private int phish_domain=-1;
-	
+
+	private int phish_domain = -1;
+
 	@Override
 	public String[] getParts() {
 		String[] parts = super.getParts().clone();
 		ArrayList<String> adder = new ArrayList<String>(Arrays.asList(parts));
-		String[] address = new String[]{parts[0],parts[1],parts[2],parts[3]};
+		String[] address = new String[] { parts[0], parts[1], parts[2],
+				parts[3] };
 		adder.set(2, "");
-		adder.set(3, PHISHER_DOMAINS[phish_domain]);
+
+		if (phish_domain < PHISHER_DOMAINS.length) {
+			//a real domain is picked, no IP
+			adder.set(3, PHISHER_DOMAINS[phish_domain]);
+		}else{
+			//IP Address to be generated for parts[3]
+			String ip = getRandomIP();
+			adder.set(3, ip);
+		}
 		adder.addAll(5, Arrays.asList(address));
 		return adder.toArray(new String[0]);
 	}
 	
+	private String getRandomIP() {
+		Random rand = BackendControllerImpl.getInstance().getRandom();
+		String result = "";
+		for (int i = 0; i < 4; i++) {
+			result += rand.nextInt(255);
+			if (i < 3) {
+				result += ".";
+			}
+		}
+		return result;
+	}
+
 	@Override
 	public List<Integer> getAttackParts() {
 		List<Integer> result = super.getAttackParts();
