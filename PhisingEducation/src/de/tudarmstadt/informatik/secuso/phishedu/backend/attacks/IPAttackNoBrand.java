da33e84dc77ff85dae9fd82efa26fdbef86ae2ba
gcanova <gamze.canova@googlemail.com>
2014-08-03 17:03:40 +0200

    IP no brand, totally unrelated no brand ensures that brand name does not occur in path part

--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/IPAttackNoBrand.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/IPAttackNoBrand.java
@@ -8,45 +8,52 @@ import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishAttackType;
 import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishURL;
 
 /**
- * This attack is baesed on using an IP address as hostname.
- * This might confuse the user so he can not be sure that it might not be the correct Host.
+ * This attack is baesed on using an IP address as hostname. This might confuse
+ * the user so he can not be sure that it might not be the correct Host.
+ * 
  * @author Clemens Bergmann <cbergmann@schuhklassert.de>
- *
+ * 
  */
 public class IPAttackNoBrand extends AbstractAttack {
 
 	/**
 	 * See {@link AbstractAttack#AbstractAttack(PhishURL)}
-	 * @param object See {@link AbstractAttack#AbstractAttack(PhishURL)}
+	 * 
+	 * @param object
+	 *            See {@link AbstractAttack#AbstractAttack(PhishURL)}
 	 */
 	public IPAttackNoBrand(PhishURL object) {
 		super(object);
-		ip=getRandomIP();
+		ip = getRandomIP();
 	}
-	
+
 	@Override
-	public String[] getParts(){
+	public String[] getParts() {
 		String[] parts = this.object.getParts();
-		parts[2] = ""; //when using IPs subdomains are not allowed.
+		parts[2] = ""; // when using IPs subdomains are not allowed.
+		
+		String secondLevelDomain = parts[3].split("\\.")[0]; //for later replacement
 		parts[3] = ip;
-		// the rest is "" for this attack
-		for(int i=4; i<parts.length; i++){
-			parts[i] = "";
+
+		// replace with "" if path contains domain
+		for (int i = 4; i < parts.length; i++) {
+			String pathWithoutDomain = parts[i].toLowerCase().replaceAll(secondLevelDomain,
+					"");
+			parts[i] = pathWithoutDomain;
 		}
-		// add a "/" to end
-		parts[4] = "/";
 
 		return parts;
 	}
-	private String ip="";
-	
-	private String getRandomIP(){
+
+	private String ip = "";
+
+	private String getRandomIP() {
 		Random rand = BackendControllerImpl.getInstance().getRandom();
 		String result = "";
-		for(int i=0; i<4; i++){
-			result+=rand.nextInt(255);
-			if(i<3){
-				result+=".";
+		for (int i = 0; i < 4; i++) {
+			result += rand.nextInt(255);
+			if (i < 3) {
+				result += ".";
 			}
 		}
 		return result;
@@ -56,7 +63,7 @@ public class IPAttackNoBrand extends AbstractAttack {
 	public PhishAttackType getAttackType() {
 		return PhishAttackType.IPNoBrand;
 	}
-	
+
 	@Override
 	public List<Integer> getAttackParts() {
 		List<Integer> result = super.getAttackParts();
--- PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/TotallyUnrelatedAttack.java
+++ PhisingEducation/src/de/tudarmstadt/informatik/secuso/phishedu/backend/attacks/TotallyUnrelatedAttack.java
@@ -8,39 +8,51 @@ import de.tudarmstadt.informatik.secuso.phishedu.backend.PhishURL;
 
 /**
  * This Attack replaces the whole URL by a replace from phishtank
+ * 
  * @author Clemens Bergmann <cbergmann@schuhklassert.de>
- *
+ * 
  */
 public class TotallyUnrelatedAttack extends AbstractAttack {
-	int attack_url=-1;
-	
+	int attack_url = -1;
+
 	protected static final String[] PHISHER_DOMAINS = SubdomainAttack.PHISHER_DOMAINS;
-	
-	protected String[] getPhisherDomains(){
+
+	protected String[] getPhisherDomains() {
 		return PHISHER_DOMAINS;
 	}
-	
+
 	/**
-	 * This constructor is required because of the implementation in {@link BackendControllerImpl#getNextUrl()}
-	 * @param object This Parmeter is discarded. It is replaced by a PhishTank URL
+	 * This constructor is required because of the implementation in
+	 * {@link BackendControllerImpl#getNextUrl()}
+	 * 
+	 * @param object
+	 *            This Parmeter is discarded. It is replaced by a PhishTank URL
 	 */
 	public TotallyUnrelatedAttack(PhishURL object) {
 		super(object);
-		attack_url=BackendControllerImpl.getInstance().getRandom().nextInt(getPhisherDomains().length);
+		attack_url = BackendControllerImpl.getInstance().getRandom()
+				.nextInt(getPhisherDomains().length);
 	}
 
 	@Override
 	public PhishAttackType getAttackType() {
-		return PhishAttackType.Subdomain;
+		return PhishAttackType.TotallyUnrelated;
 	}
-	
+
 	@Override
 	public String[] getParts() {
 		String[] parts = super.getParts();
+		String secondLevelDomain = parts[3].split("\\.")[0];
 		parts[3] = getPhisherDomains()[attack_url];
+
+		//replace with "" if path contains domain
+		for(int i = 4; i<parts.length; i++){
+			String pathWithoutDomain = parts[i].toLowerCase().replaceAll(secondLevelDomain,"");
+			parts[i] = pathWithoutDomain;
+		}
 		return parts;
 	}
-	
+
 	@Override
 	public List<Integer> getAttackParts() {
 		List<Integer> result = super.getAttackParts();
